f6f9aa391bf046fb8bbb5b7d0200abcbea54c1f5
sajclarke <shannonajclarke@gmail.com>
2021-03-04 05:59:03 -0400

    feat: Add todo list and text input field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    //Check if localstorage is
+    const storedTasks = JSON.parse(window.localStorage.getItem("tasks"));
+    console.log(storedTasks);
+    if (storedTasks && storedTasks.length > 0) {
+      setTasks(storedTasks);
+    }
+  }, []);
+
+  //UseEffect re-renders application whenever dependency objects are changed
+  useEffect(() => {
+    //Save to localstorage whenever tasks is updated
+    if (tasks.length > 0) {
+      console.log("save tasks to localstorage");
+      window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
+
+  //Update the state object whenever the field is changed
+  const handleFieldChange = (e) => {
+    const { value } = e.target;
+    setTitle(value);
+  };
+
+  //Handles saving to the tasks array
+  const handleSubmit = () => {
+    console.log("handle submit", title);
+    setTasks([...tasks, title]);
+    setTitle("");
+  };
+
+  const handleEdit = () => {
+    //TODO: Edit todo using the es6 find
+  };
+
+  const handleRemove = () => {
+    //TODO: Remove todo using es6 filter
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          name="task_title"
+          value={title}
+          placeholder="Add task here"
+          onChange={handleFieldChange}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Add task
+        </button>
+      </div>
+
+      <ul>
+        {tasks?.length > 0
+          ? tasks.map((item, index) => (
+              <li key={index}>
+                {item}
+                <button type="button" onClick={handleEdit}>
+                  Edit
+                </button>
+                <button type="button" onClick={handleRemove}>
+                  Delete
+                </button>
+              </li>
+            ))
+          : "Nothing in list"}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
